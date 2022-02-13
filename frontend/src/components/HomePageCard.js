7a4193bfa7e3d4fcfb418ed7b9c85ddcad846f43
wipada-romporee <82120336+wipada-romporee@users.noreply.github.com>
2022-02-13 22:33:27 +0700

    Homepage Card
    layout and CSS Adjustment

--- frontend/src/components/HomePageCard.js
+++ frontend/src/components/HomePageCard.js
@@ -4,41 +4,54 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import { IconButton } from "@mui/material";
 import SvgIcon from "@mui/material/SvgIcon";
 import { Icon } from "@mui/material";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import SchoolIcon from '@mui/icons-material/School';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import SchoolIcon from "@mui/icons-material/School";
 import { Avatar } from "@mui/material";
 import { Box, typography } from "@mui/system";
 import ShowReview from "./ShowReview";
 import { TextField } from "@mui/material";
 
 const price = "200 B/ 15 min";
-const ConsultantName ="Consultant Name";
+const ConsultantName = "Consultant Name";
 const Department = "Tag name";
-const Hospital = "SomeWhere" ;
+const Hospital = "SomeWhere";
 const College = "KMUTNB";
-const Details = "Some Details"
+const Details = "Some Details";
 export default function HomePageCard() {
   return (
     <>
-      <Container>
-        <IconButton aria-label="Example">
-          <SvgIcon component={FavoriteIcon} />
-        </IconButton>
-        <label>{price}</label>
-        <Avatar src="/broken-image.jpg" />
+      <Box
+        sx={{
+          border: 1,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          maxWidth: "fit-content",
+          padding: 3,
+          margin: 3
+        }}
+      >
+        <Box sx={{width:"100%",display:"flex",alignItems: "center",justifyContent:"space-between"}}>
+          <IconButton aria-label="Example">
+            <SvgIcon component={FavoriteIcon} />
+          </IconButton>
+          <label>{price}</label>
+        </Box>
+
+        <Avatar sx={{ width: 60, height: 60 }} src="/broken-image.jpg" />
         <typography>{ConsultantName}</typography>
         <Chip label={Department}></Chip>
-        <ShowReview/>
+        <ShowReview />
 
-        <Box>
-        <div>
-        <LocationOnIcon/>
-        <typography>{Hospital}</typography>
-        </div>
-        <div>
-         <SchoolIcon />
-         <typography>{College}</typography>   
-        </div>
+        <Box sx={{width:"100%",display:"flex",alignItems: "center",justifyContent:"space-between"}}>
+          <div>
+            <LocationOnIcon />
+            <typography>{Hospital}</typography>
+          </div>
+          <div>
+            <SchoolIcon />
+            <typography>{College}</typography>
+          </div>
         </Box>
 
         <TextField
@@ -53,8 +66,7 @@ export default function HomePageCard() {
 
         <Button>จองล่วงหน้า</Button>
         <Button>ปรึกษาทันที</Button>
-        
-      </Container>
+      </Box>
     </>
   );
 }
