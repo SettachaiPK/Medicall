f2215b7d0d417d2c8fa8451401712331abccd536
wipada-romporee <82120336+wipada-romporee@users.noreply.github.com>
2022-02-18 01:26:41 +0700

    Customer Consultant Details page

--- frontend/src/views/Customer/ConsultantDetailPage.js
+++ frontend/src/views/Customer/ConsultantDetailPage.js
@@ -4,6 +4,10 @@ import { ConsultantDetailModel } from "../../models";
 import { getConsultantDetail } from "../../service/customer.service";
 import { Box } from "@mui/system";
 import { Avatar } from "@mui/material";
+import MessageIcon from "@mui/icons-material/Message";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import CallIcon from "@mui/icons-material/Call";
+import { Button } from "@mui/material";
 
 function ConsultantDetailPage() {
   const { id } = useParams();
@@ -23,6 +27,8 @@ function ConsultantDetailPage() {
     avatar,
   } = consultant;
 
+  const review = "review";
+
   const fetchDetail = useCallback(async () => {
     const { data } = await getConsultantDetail(id);
     setConsultant(data);
@@ -33,27 +39,97 @@ function ConsultantDetailPage() {
   }, [fetchDetail]);
 
   return (
-    <div>
-      <Avatar src={`data:image/png;base64, ${avatar}`} />
-      <div className="flex">
-        {firstName} {lastName}
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}>
+      <div className="flex_column_center">
+        <Avatar
+          sx={{ width: 60, height: 60 }}
+          src={`data:image/png;base64, ${avatar}`}
+        />
+        <div className="flex_column_center">
+          <label>{firstName}</label>
+          <label>{lastName}</label>
+        </div>
+        <div>{onlineStatus}</div>
       </div>
       <br />
-      <div>status: {onlineStatus}</div>
-      <br />
-      <Box sx={{ border: 1, maxWidth: "fit-content",padding: "2%" }}>
-      <div>
-      <div>department: {department}</div>
-      <div>infirmary: {infirmary}</div>
-      <div>academy: {academy}</div> 
+      <div style={{ marginLeft: "4%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            border: 1,
+            width: "max-content",
+            padding: "5%",
+          }}
+        >
+          <div>
+            <div>department: {department}</div>
+            <div>infirmary: {infirmary}</div>
+            <div>academy: {academy}</div>
+          </div>
+          <div style={{ marginLeft: "150px", marginTop: "25px" }}>
+            AvrgReview
+          </div>
+        </Box>
+        <br />
+        <Box
+          sx={{
+            minWidth: 400,
+            maxHeight: 200,
+            border: 1,
+            padding: "5%",
+          }}
+        >
+          <Box
+            sx={{
+              minWidth: 200,
+              maxWidth: 350,
+              minHeight: 50,
+              maxHeight: 70,
+              margin: "auto",
+              border: 1,
+              padding: "3%",
+            }}
+          >
+            <div>detail: {detail}</div>
+          </Box>
+          <Box
+            sx={{ display: "flex", justifyContent: "space-around", margin: 2 }}
+          >
+            <div style={{ display: "grid" }}>
+              <MessageIcon />
+              {messagePrice}
+            </div>
+            <div style={{ display: "grid" }}>
+              <VideocamIcon /> {voiceCallPrice}
+            </div>
+            <div style={{ display: "grid" }}>
+              <CallIcon /> {videoCallPrice}
+            </div>
+          </Box>
+        </Box>
+        <br />
+        <Box
+          sx={{
+            minWidth: 400,
+            maxHeight: 200,
+            border: 1,
+            padding: "5%",
+          }}
+        >
+          <Box
+            sx={{
+              minWidth: 50,
+              border: 1,
+              maxWidth: 50,
+              padding: "5%",
+            }}
+          >
+            <div>{review}</div>
+          </Box>
+        </Box>
+        <Button>จองล่วงหน้า</Button>
+        <Button>ปรึกษาทันที</Button>
       </div>
-      <div>Review</div>
-      </Box>
-      <br />
-      <div>detail: {detail}</div>
-      <div>messagePrice: {messagePrice}</div>
-      <div>voiceCallPrice: {voiceCallPrice}</div>
-      <div>videoCallPrice: {videoCallPrice}</div>
     </div>
   );
 }
