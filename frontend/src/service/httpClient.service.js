5a17a450d3791d986817c3779492ae9ecff599e8
SettachaiPK <64306985+SettachaiPK@users.noreply.github.com>
2022-05-22 04:08:55 +0700

    check node env

--- frontend/src/service/httpClient.service.js
+++ frontend/src/service/httpClient.service.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-import { API_URL, NOT_CONNECT_NETWORK, NETWORK_CONNECTION_MESSAGE } from './constants.service';
-import { checkRefreshToken, signOut } from './auth.service';
+import {
+  API_URL,
+  NOT_CONNECT_NETWORK,
+  NETWORK_CONNECTION_MESSAGE,
+} from "./constants.service";
+import { checkRefreshToken, signOut } from "./auth.service";
 import join from "url-join";
 
 const isAbsoluteURLRegex = /^(?:\w+:)\/\//;
@@ -9,70 +13,76 @@ let isRefreshing = false;
 let checked = false;
 
 const processQueue = () => {
-    failedQueue.forEach(prom => {
-        axios(prom)
-    })
-    failedQueue = [];
-}
+  failedQueue.forEach((prom) => {
+    axios(prom);
+  });
+  failedQueue = [];
+};
 
-axios.interceptors.request.use( request => {
-    let Config = request.config
+axios.interceptors.request.use((request) => {
+  let Config = request.config;
 
-    if (!isAbsoluteURLRegex.test(request.url)) {
-        console.log(API_URL)
-        if (Config.param !== null) request.url = join(API_URL, request.url, Config.param)
-        else request.url = join(API_URL, request.url)
-    }
+  if (!isAbsoluteURLRegex.test(request.url)) {
+    console.log(process.env.NODE_ENV, API_URL);
+    if (Config.param !== null)
+      request.url = join(API_URL, request.url, Config.param);
+    else request.url = join(API_URL, request.url);
+  }
 
-    return request
-})
+  return request;
+});
 
 axios.interceptors.response.use(
-    response => {
-        return response
-    },
-    error => {
-        let originalRequest = error.config;
+  (response) => {
+    return response;
+  },
+  (error) => {
+    let originalRequest = error.config;
 
-        if (error.response.status === 403 && error.response.data.message === "No token provided!" && !originalRequest._retry) { 
-            if ( isRefreshing ) {
-                failedQueue.push(originalRequest)
-            }
+    if (
+      error.response.status === 403 &&
+      error.response.data.message === "No token provided!" &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        failedQueue.push(originalRequest);
+      }
 
-            originalRequest._retry = true;
-            isRefreshing = true;
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-            if ( !checked ) {
-                return new Promise(function (resolve, reject) {
-                    checked = true
-                    checkRefreshToken()
-                    .then(({data}) => {
-                        checked = false;
-                        processQueue();
-                        resolve(axios(originalRequest));
-                    })
-                    .catch((err) => {
-                        checked = false;
-                        signOut();
-                        localStorage.removeItem(`${process.env.REACT_APP_PREFIX}_USER`);
-                        failedQueue = []
-                        reject(err)
-                    })
-                    .finally(() => { isRefreshing = false })
-                })
-            }
-        } else if (axios.isCancel(error)) {
-            return Promise.reject(error);
-        } else if (!error.response) {
-            return Promise.reject({
-                code: NOT_CONNECT_NETWORK,
-                message: NETWORK_CONNECTION_MESSAGE,
+      if (!checked) {
+        return new Promise(function (resolve, reject) {
+          checked = true;
+          checkRefreshToken()
+            .then(({ data }) => {
+              checked = false;
+              processQueue();
+              resolve(axios(originalRequest));
+            })
+            .catch((err) => {
+              checked = false;
+              signOut();
+              localStorage.removeItem(`${process.env.REACT_APP_PREFIX}_USER`);
+              failedQueue = [];
+              reject(err);
+            })
+            .finally(() => {
+              isRefreshing = false;
             });
-        }
-        else return Promise.reject(error)
+        });
+      }
+    } else if (axios.isCancel(error)) {
+      return Promise.reject(error);
+    } else if (!error.response) {
+      return Promise.reject({
+        code: NOT_CONNECT_NETWORK,
+        message: NETWORK_CONNECTION_MESSAGE,
+      });
+    } else return Promise.reject(error);
 
-        return Promise.reject(error)
-    }
-)
+    return Promise.reject(error);
+  }
+);
 
 export default axios;
